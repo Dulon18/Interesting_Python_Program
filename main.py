34f8cbe9ff392c9301121fcc07cfef56d2ab7aa0
Dulon <80118217+Dulon18@users.noreply.github.com>
2021-05-16 02:12:52 +0600

    Add files via upload
    Text To Handwritten

---
+++ main.py
@@ -0,0 +1,2 @@
+import pywhatkit
+pywhatkit.text_to_handwriting("Python is a high-level, interpreted, interactive and object-oriented scripting language. Python is designed to be highly readable. It uses English keywords frequently where as other languages use punctuation, and it has fewer syntactical constructions than other languages." ,rgb=(0,0,255))
