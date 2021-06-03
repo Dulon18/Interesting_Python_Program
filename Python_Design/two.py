3208de0bf5c84c3d549a9b29a3c90fedc77d3271
Dulon <80118217+Dulon18@users.noreply.github.com>
2021-06-03 22:59:24 +0600

    Create two.py

---
+++ Python_Design/two.py
@@ -0,0 +1,18 @@
+#pip install turtle
+import turtle
+
+t = turtle.Turtle()
+turtle.bgcolor("black")
+turtle.pensize(2)
+turtle.speed(0)
+
+
+while(True):
+    for i in range(6):
+        for colors in ['red', 'purple', 'blue', 'green', 'orange', 'yellow']:
+            turtle.color(colors)
+            turtle.circle(100)
+            turtle.left(10)
+
+turtle.hideturtle()
+turtle.mainloop()
