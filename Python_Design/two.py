79cc24a83c4d22fb24f51d0d283b034ececb43bf
Dulon <80118217+Dulon18@users.noreply.github.com>
2021-06-03 23:20:40 +0600

    Update two.py

--- Python_Design/two.py
+++ Python_Design/two.py
@@ -9,10 +9,10 @@ turtle.speed(0)
 
 while(True):
     for i in range(6):
-        for colors in ['red', 'purple', 'blue', 'green', 'orange', 'yellow']:
+        for colors in ['green', 'orange']:
             turtle.color(colors)
             turtle.circle(100)
-            turtle.left(10)
+            turtle.left(4)
 
 turtle.hideturtle()
 turtle.mainloop()
